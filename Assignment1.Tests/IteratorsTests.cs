3b5047e0ceb34a2d566d218f95739c38d75ab143
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-09-09 13:32:17 +0200

    Added more test cases

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -7,17 +7,40 @@ public class IteratorsTests
     [Fact]
     public void Flatten_EmptyList_ReturnsEmptyList()
     {
+        // Arrange
         var list = new List<List<int>>();
+
+        // Act
         var result = Iterators.Flatten(list);
+
+        // Assert
         Assert.Empty(result);
     }
 
     [Fact]
     public void Flatten_ListWithList_1_2_3_4_Returns_List_1_2_3_4()
     {
+        // Arrange
         var list = new List<List<int>> { new List<int>() { 1, 2, 3, 4 } };
+
+        // Act
         var result = Iterators.Flatten(list);
+
+        // Assert
         Assert.Equal(new[] { 1, 2, 3, 4 }, result);
+    }
+
+    [Fact]
+    public void Flatten_ListWithList_1_2_3_4_And_List_5_6_7_8_Returns_List_1_2_3_4_5_6_7_8()
+    {
+        // Arrange
+        var list = new List<List<int>> { new List<int>() { 1, 2, 3, 4 }, new List<int>() { 5, 6, 7, 8 } };
+
+        // Act
+        var result = Iterators.Flatten(list);
+
+        // Assert
+        Assert.Equal(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, result);
     }
 
 }
